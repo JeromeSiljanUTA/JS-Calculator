548cc4ad37060fe5c6dc543498eb91aa0d90aca9
Jerome Siljan <jerome.siljan@mavs.uta.edu>
2022-05-01 15:58:50 -0500

    calculator working with basic operations

--- backend.js
+++ backend.js
@@ -1,8 +1,18 @@
-var toggleVal = "hide"; total = 0;
+var toggleVal = "hide", num1 = 0, num2 = 0, operation = "", clearNow = false;
 
 function append(num) {
-    total = (total * 10) + num;
-    document.getElementById('view').innerHTML = total;
+    if(clearNow == true){
+        clearView();
+        clearNow = false;
+    }
+    if(operation == ""){
+        num1 = (num1 * 10) + num;
+        document.getElementById('view').innerHTML = num1;
+    }
+    else{
+        num2 = (num2 * 10) + num;
+        document.getElementById('view').innerHTML = num1 + " " + operation + " " + num2;
+    }
 }
 
 function toggleVisibility() {
@@ -18,10 +28,31 @@ function toggleVisibility() {
 }
 
 function clearView() {
-    total = 0;
-    document.getElementById('view').innerHTML = total;
+    num1 = 0;
+    num2 = 0;
+    operation = "";
+    document.getElementById('view').innerHTML = num1;
+}
+
+function operator(input_operator){
+    operation = input_operator;
+    document.getElementById('view').innerHTML = num1 + " " + input_operator;
 }
 
 function calculate(){
-    window.alert('calculate');
+    var result;
+    if(operation == "+"){
+        result = num1 + num2;
+    }
+    else if(operation == "-"){
+        result = num1 - num2;
+    }
+    else if(operation == "*"){
+        result = num1 * num2;
+    }
+    else if(operation == "/"){
+        result = num1 / num2;
+    }
+    document.getElementById('view').innerHTML = result;
+    clearNow = true;
 }
